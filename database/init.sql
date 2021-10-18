32d7c68e01132450b0aa7ef452e116a3dfa4fb0d
“Moggach” <acunnane13@gmail.com>
2021-10-18 16:32:43 +0100

    adding files for initial sql database
    Co-authored-by: Michael <72146072+MJOW1999@users.noreply.github.com>

---
+++ database/init.sql
@@ -0,0 +1,32 @@
+BEGIN;
+
+DROP TABLE IF EXISTS users, sessions CASCADE;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  email TEXT NOT NULL UNIQUE,
+  name TEXT NOT NULL,
+  avatar BYTEA NOT NULL
+  
+);
+
+CREATE TABLE sessions (
+   sid TEXT PRIMARY KEY,
+   data JSON NOT NULL
+);
+  
+  
+INSERT INTO users ( email, name, avatar) VALUES
+(
+  'test@gmail.com',
+  'Initial User',
+  'http://placekitten.com/200/300'
+);
+
+INSERT INTO sessions (sid, data) VALUES
+(
+  '1234',
+  '{"test":"testing cookie"}'
+);
+
+COMMIT;
