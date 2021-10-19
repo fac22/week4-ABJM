893abc6b7503be553ed5143a8c94d52fe321d180
MJOW1999 <michael.jow.williams@gmail.com>
2021-10-19 11:41:27 +0100

    Made createUser function in auth.js and model.js
    Co-authored-by: jijip41 <78619809+jijip41@users.noreply.github.com>

--- auth.js
+++ auth.js
@@ -1,30 +1,36 @@
-const crypto = require('crypto');
-const bcrypt = require('bcryptjs');
-const model = require('./database/model');
+const crypto = require("crypto");
+const bcrypt = require("bcryptjs");
+const model = require("./database/model");
 
 const COOKIE_OPTIONS = {
-	httpOnly: true,
-	maxAge: 600000,
-	sameSite: 'lax',
-	signed: true,
+  httpOnly: true,
+  maxAge: 600000,
+  sameSite: "lax",
+  signed: true,
 };
 
 function verifyUser(email, password) {
-	return model.getUser(email).then(user => {
-		return bcrypt.compare(password, user.password).then(match => {
-			if (!match) {
-				throw new Error("password doesn't match!");
-			} else {
-				delete user.password;
-				return user;
-			}
-		});
-	});
+  return model.getUser(email).then((user) => {
+    return bcrypt.compare(password, user.password).then((match) => {
+      if (!match) {
+        throw new Error("password doesn't match!");
+      } else {
+        delete user.password;
+        return user;
+      }
+    });
+  });
+}
+
+function createUser(name, email, password, avatar) {
+  return bcrypt
+    .hash(password, 10)
+    .then((hash) => model.createUser(name, email, hash, avatar));
 }
 
 function saveUserSession(user) {
-	const sid = crypto.randomBytes(18).toString('base64');
-	return model.createSession(sid, { user });
+  const sid = crypto.randomBytes(18).toString("base64");
+  return model.createSession(sid, { user });
 }
 
-module.exports = { verifyUser, saveUserSession, COOKIE_OPTIONS };
+module.exports = { verifyUser, saveUserSession, createUser, COOKIE_OPTIONS };
--- database/model.js
+++ database/model.js
@@ -1,15 +1,22 @@
-const db = require('./connection.js');
+const db = require("./connection.js");
 
 function getUser(email) {
-	const SELECT_USER = `SOMEEEETHINNNG`;
-	return db.query(SELECT_USER, [email]).then(result => result.rows[0]);
+  const SELECT_USER = `SOMEEEETHINNNG`;
+  return db.query(SELECT_USER, [email]).then((result) => result.rows[0]);
+}
+
+function createUser(name, email, hash, avatar) {
+  const INSERT_USER = `INSERT INTO users (name, email, password, avatar) VALUES ($1, $2, $3, $4)`;
+  return db
+    .query(INSERT_USER, [name, email, hash, avatar])
+    .then((result) => result.rows[0]);
 }
 
 function createSession(sid, json) {
-	const INSERT_SESSION = `SOMEEETHINNNG`;
-	return db
-		.query(INSERT_SESSION, [sid, json])
-		.then(response => response.rows[0].sid);
+  const INSERT_SESSION = `SOMEEETHINNNG`;
+  return db
+    .query(INSERT_SESSION, [sid, json])
+    .then((response) => response.rows[0].sid);
 }
 
-module.exports = { getUser, createSession };
+module.exports = { getUser, createSession, createUser };
