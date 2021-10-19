1446ddd184ebf127c38cdd88ab36f47b0c83c28e
“Moggach” <acunnane13@gmail.com>
2021-10-19 11:37:31 +0100

    finishing login page with submit button, some client side validation and sql queries
    Co-authored-by: Barbara <77367593+0bubbles0@users.noreply.github.com>

--- database/model.js
+++ database/model.js
@@ -1,15 +1,19 @@
 const db = require('./connection.js');
 
 function getUser(email) {
-	const SELECT_USER = `SOMEEEETHINNNG`;
-	return db.query(SELECT_USER, [email]).then(result => result.rows[0]);
+  const SELECT_USER = `
+    SELECT id, email, password, name FROM users WHERE email=$1
+    `;
+  return db.query(SELECT_USER, [email]).then((result) => result.rows[0]);
 }
 
 function createSession(sid, json) {
-	const INSERT_SESSION = `SOMEEETHINNNG`;
-	return db
-		.query(INSERT_SESSION, [sid, json])
-		.then(response => response.rows[0].sid);
+  const INSERT_SESSION = `
+  INSERT INTO sessions (sid, data) VALUES ($1, $2)
+  RETURNING sid`;
+  return db
+    .query(INSERT_SESSION, [sid, json])
+    .then((response) => response.rows[0].sid);
 }
 
 module.exports = { getUser, createSession };
