104f129a978fc1444ae61d330c517c5f20761e68
bubbles linux <appenzeller.barbara@gmail.com>
2021-10-20 18:07:25 +0100

    My recipes: add SQL, get-function in recipesMine.js, model.js, server.js. There's problem with sid-->getSession
    Co-authored-by: moggach <43313455+Moggach@users.noreply.github.com>
    Co-authored-by: jijip41 <78619809+jijip41@users.noreply.github.com>
    Co-authored-by: Michael <72146072+MJOW1999@users.noreply.github.com>

--- database/model.js
+++ database/model.js
@@ -1,44 +1,44 @@
 const db = require('./connection.js');
 
 function getUser(email) {
-  const SELECT_USER = `
+	const SELECT_USER = `
     SELECT id, email, password, name, avatar FROM users WHERE email=$1
     `;
 
-  return db.query(SELECT_USER, [email]).then((result) => result.rows[0]);
+	return db.query(SELECT_USER, [email]).then(result => result.rows[0]);
 }
 
 function createUser(name, email, hash, avatar) {
-  const INSERT_USER = `INSERT INTO users (name, email, password, avatar) VALUES ($1, $2, $3, $4) 
+	const INSERT_USER = `INSERT INTO users (name, email, password, avatar) VALUES ($1, $2, $3, $4) 
   RETURNING id, name, email;`;
-  return db.query(INSERT_USER, [name, email, hash, avatar]).then((result) => {
-    return result.rows[0];
-  });
+	return db.query(INSERT_USER, [name, email, hash, avatar]).then(result => {
+		return result.rows[0];
+	});
 }
 
 function createSession(sid, json) {
-  const INSERT_SESSION = `INSERT INTO sessions (sid, data) VALUES ($1, $2)
+	const INSERT_SESSION = `INSERT INTO sessions (sid, data) VALUES ($1, $2)
 	RETURNING sid;`;
-  return db
-    .query(INSERT_SESSION, [sid, json])
-    .then((response) => response.rows[0].sid);
+	return db
+		.query(INSERT_SESSION, [sid, json])
+		.then(response => response.rows[0].sid);
 }
 
 function deleteSession(sid) {
-  const DELETE_SESSION = 'DELETE FROM sessions WHERE sid=$1';
-  return db.query(DELETE_SESSION, [sid]);
+	const DELETE_SESSION = 'DELETE FROM sessions WHERE sid=$1';
+	return db.query(DELETE_SESSION, [sid]);
 }
 
 function getSession(sid) {
-  const GET_SESSION = `SELECT data FROM sessions WHERE sid = $1`;
-  return db.query(GET_SESSION, [sid]).then((result) => {
-    const singleResult = result.rows[0];
-    return singleResult && singleResult.data;
-  });
+	const GET_SESSION = `SELECT data FROM sessions WHERE sid = $1`;
+	return db.query(GET_SESSION, [sid]).then(result => {
+		const singleResult = result.rows[0];
+		return singleResult && singleResult.data;
+	});
 }
 
 function showRecipes() {
-  const query = `SELECT 
+	const query = `SELECT 
   recipes.title, 
   recipes.ingredients, 
   recipes.instructions,
@@ -46,28 +46,30 @@ function showRecipes() {
 FROM recipes 
 JOIN users 
 ON recipes.user_id = users.id`;
-  return db.query(query).then((result) => result.rows);
+	return db.query(query).then(result => result.rows);
 }
 
 function showMyRecipes(email) {
-  const query = `SELECT 
-  recipes.title, 
-  recipes.ingredients, 
-  recipes.instructions,
-  users.name
-FROM recipes 
-JOIN users 
-ON recipes.user_id = users.id
-WHERE email = $1`;
-  return db.query(query, [email]).then((result) => result.rows);
+	const query = `
+  SELECT 
+    recipes.title, 
+    recipes.ingredients, 
+    recipes.instructions,
+    users.name
+  FROM recipes 
+  JOIN users 
+  ON recipes.user_id = users.id
+  WHERE email = $1
+  `;
+	return db.query(query, [email]).then(result => result.rows);
 }
 
 module.exports = {
-  getUser,
-  createUser,
-  createSession,
-  deleteSession,
-  getSession,
-  showRecipes,
-  showMyRecipes,
+	getUser,
+	createUser,
+	createSession,
+	deleteSession,
+	getSession,
+	showRecipes,
+	showMyRecipes,
 };
--- server.js
+++ server.js
@@ -6,18 +6,17 @@ const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 3000;
 
 const home = require('./routes/home.js');
-const recipeWrite = require('./routes/recipeWrite.js');
 
 const server = express();
 
+const signUp = require('./routes/signUp.js');
+const logIn = require('./routes/logIn.js');
+const logOut = require('./routes/logOut.js');
+const recipeWrite = require('./routes/recipeWrite.js');
+const recipesMine = require('./routes/recipesMine.js');
 // const recipesAll = require('./routes/recipesAll.js');
 // const recipeDelete = require('./routes/recipeDelete.js');
 // const recipeEdit = require('./routes/recipeEdit.js');
-// const recipesMine = require('./routes/recipesMine.js');
-// const recipePost = require('./routes/recipePost.js');
-const logIn = require('./routes/logIn.js');
-// const logOut = require('./routes/logOut.js');
-const signUp = require('./routes/signUp.js');
 // const userDelete = require('./routes/userDelete.js');
 // const userEdit = require('./routes/userEdit.js');
 
@@ -27,6 +26,14 @@ server.use(cookieParser(process.env.COOKIE_SECRET));
 
 server.get('/', home.get);
 
+server.get('/signUp', signUp.get);
+server.post('/signUp', signUp.post);
+
+server.get('/logIn', logIn.get);
+server.post('/logIn', logIn.post);
+
+server.post('/logOut', logOut.post);
+
 server.get('/recipeWrite', recipeWrite.get);
 server.post('/recipeWrite', recipeWrite.post);
 
@@ -35,26 +42,14 @@ server.post('/recipeWrite', recipeWrite.post);
 // server.get('/recipesAll', recipesAll.get);
 // server.post('/recipesAll', recipesAll.post);
 
+server.get('/recipesMine', recipesMine.get);
+// server.post('/recipesMine', recipesMine.post);
+
 // server.get('/recipeDelete', recipeDelete.get);
 // server.post('/recipeDelete, recipeDelete.post);
 
 // server.get('/recipeEdit', recipeEdit.get);
 // server.post('/recipeEdit', recipeEdit.post);
-
-// server.get('/recipesMine', recipesMine.get);
-// server.post('/recipesMine', recipesMine.post);
-
-// server.get('/recipePost', recipePost.get);
-// server.post('/recipePost', recipePost.post);
-
-server.get('/logIn', logIn.get);
-server.post('/logIn', logIn.post);
-
-// server.get('/logOut', logOut.get);
-// server.post('/logOut', logOut.post);
-
-server.get('/signUp', signUp.get);
-server.post('/signUp', signUp.post);
 
 // /*server.get('/userDelete', userDelete.get);
 // server.post('/userDelete', userDelete.post);
