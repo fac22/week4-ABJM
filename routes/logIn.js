8c608e1de56a9b2b6582c33333b10aedb8df489d
bubbles linux <appenzeller.barbara@gmail.com>
2021-10-21 15:28:59 +0100

    logIn server-validation
    Co-authored-by: Jihye Pak <jijip41@users.noreply.github.com>

--- routes/logIn.js
+++ routes/logIn.js
@@ -3,9 +3,9 @@ const auth = require('../auth.js');
 const { buildPage } = require('../template.js');
 
 function get(request, response) {
-  // Login form
-  const title = 'Log in';
-  const form = /*html*/ `
+	// Login form
+	const title = 'Log in';
+	const form = /*html*/ `
 		<h1>Log in</h1>
 		<form action="/logIn" method="POST">
         <div class="flex">
@@ -22,36 +22,40 @@ function get(request, response) {
       </div>
 		</form>
 		`;
-  response.send(buildPage(title, form));
+	response.send(buildPage(title, form));
 }
 
 function post(request, response) {
-  // const {email, password} = request.body;
-  const { email, password } = request.body;
-  //match passwords
-  //make new session with sid
-  auth
-    // match passwords
-    .verifyUser(email, password)
-    // make new session with session id
-    .then(auth.saveUserSession)
-    // make a cookie
-    .then((sid) => {
-      response.cookie('sid', sid, auth.COOKIE_OPTIONS);
-      response.redirect('/');
-    })
-    // catch
-    .catch((error) => {
-      console.error(error);
-      response.send(
-        buildPage(
-          `Error`,
-          /*html*/ `<h2>User not found</h2> <div>
-          <a href="/">Go Back</a>
-      </div>`
-        )
-      );
-    });
+	// const {email, password} = request.body;
+	const { email, password } = request.body;
+
+	const errorTitle = `Error`;
+	const errorContent = /*html*/ `<h2>Couldn't log in, sorry</h2>
+  <div class="centre"><a href="/">Go Home</a><a href="/logIn">Go Log In</a></div>`;
+	const errorPage = buildPage(errorTitle, errorContent);
+
+	// This is server-side validation:
+	if (!email || !password) {
+		response.send(errorPage);
+	} else {
+		//match passwords
+		//make new session with sid
+		auth
+			// match passwords
+			.verifyUser(email, password)
+			// make new session with session id
+			.then(auth.saveUserSession)
+			// make a cookie
+			.then((sid) => {
+				response.cookie('sid', sid, auth.COOKIE_OPTIONS);
+				response.redirect('/');
+			})
+			// catch
+			.catch((error) => {
+				console.error(error);
+				response.send(errorPage);
+			});
+	}
 }
 
 module.exports = { get, post };
