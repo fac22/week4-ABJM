c97a878779088fb9766910a1552103fc017a9c22
jihye <jiji.pak41@gmail.com>
2021-10-21 20:41:30 +0100

    cstyle error messages
    Co-authored-by: moggach <43313455+Moggach@users.noreply.github.com>
    Co-authored-by: Michael <72146072+MJOW1999@users.noreply.github.com>
    Co-authored-by: Barbara <77367593+0bubbles0@users.noreply.github.com>

--- routes/logIn.js
+++ routes/logIn.js
@@ -3,23 +3,23 @@ const auth = require('../auth.js');
 const { buildPage } = require('../template.js');
 
 function get(request, response) {
-	// Login form
-	const title = 'Log in';
-	const form = /*html*/ `
+  // Login form
+  const title = 'Log in';
+  const form = /*html*/ `
 		<h1>Log in</h1>
 		<form action="/logIn" method="POST">
         <div class="flex">
 		<label for="email">Email<span aria-hidden="true">*</span></label>
 		<input type="email" id="email" name="email" placeholder="Enter your email" required>
 	    </div>
-      <p id="emailRequirements" class="requirements centre">
+      <p id="emailRequirements" class="requirements">
         Please remember your email.
       		</p>
 		<div class="flex">
 		<label for="password">Password<span aria-hidden="true">*</span></label>
 		<input placeholder="Enter your password" type="password" id="password" name="password" required>
 	    </div>
-      <p id="passwordRequirements" class="requirements centre">
+      <p id="passwordRequirements" class="requirements">
       Please remember your password.
       		</p>
       <div class="centre">
@@ -28,40 +28,40 @@ function get(request, response) {
       </div>
 		</form>
 		`;
-	response.send(buildPage(title, form));
+  response.send(buildPage(title, form));
 }
 
 function post(request, response) {
-	// const {email, password} = request.body;
-	const { email, password } = request.body;
+  // const {email, password} = request.body;
+  const { email, password } = request.body;
 
-	const errorTitle = `Error`;
-	const errorContent = /*html*/ `<h2>Couldn't log in, sorry</h2>
+  const errorTitle = `Error`;
+  const errorContent = /*html*/ `<h2>Couldn't log in, sorry</h2>
   <div class="centre"><a href="/">Go Home</a><a href="/logIn">Go Log In</a></div>`;
-	const errorPage = buildPage(errorTitle, errorContent);
+  const errorPage = buildPage(errorTitle, errorContent);
 
-	// This is server-side validation:
-	if (!email || !password) {
-		response.send(errorPage);
-	} else {
-		//match passwords
-		//make new session with sid
-		auth
-			// match passwords
-			.verifyUser(email, password)
-			// make new session with session id
-			.then(auth.saveUserSession)
-			// make a cookie
-			.then((sid) => {
-				response.cookie('sid', sid, auth.COOKIE_OPTIONS);
-				response.redirect('/');
-			})
-			// catch
-			.catch((error) => {
-				console.error(error);
-				response.send(errorPage);
-			});
-	}
+  // This is server-side validation:
+  if (!email || !password) {
+    response.send(errorPage);
+  } else {
+    //match passwords
+    //make new session with sid
+    auth
+      // match passwords
+      .verifyUser(email, password)
+      // make new session with session id
+      .then(auth.saveUserSession)
+      // make a cookie
+      .then((sid) => {
+        response.cookie('sid', sid, auth.COOKIE_OPTIONS);
+        response.redirect('/');
+      })
+      // catch
+      .catch((error) => {
+        console.error(error);
+        response.send(errorPage);
+      });
+  }
 }
 
 module.exports = { get, post };
