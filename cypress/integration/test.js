e265802fbb9c63af1f452e5f43efedf204c3a669
bubbles linux <appenzeller.barbara@gmail.com>
2021-10-21 11:45:56 +0100

    comment back in tests

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -2,6 +2,7 @@ beforeEach(() => {
 	cy.task('resetDb');
 });
 
+// Practice Test
 // describe('Testing Works', () => {
 // 	it('Passes when visiting google', () => {
 // 		cy.visit('www.google.com');
@@ -10,6 +11,7 @@ beforeEach(() => {
 
 /*
 Routes:
+Done:
 - home unknown: login/sign-up button, all recipes
 - signUp
 - logout
@@ -18,6 +20,7 @@ Routes:
 - recipeWrite
 - recipesMine
 
+Still to do:
 - recipeEdit
 - recipeDelete
 - userProfile
@@ -25,85 +28,85 @@ Routes:
 - userDelete
 */
 
-// // Routes:
-// // home newbie: login/sign-up button, all recipes
-// describe('Homepage for newbies', () => {
-// 	it('Displays Sign up and Log in', () => {
-// 		cy.visit('/');
-// 		//has <a href="">
-// 		cy.contains('Sign up');
-// 		cy.contains('Log in');
-// 	});
-// 	it('Shows all recipes', () => {
-// 		// cy.visit('/');
-// 		cy.contains('Strawberry Jam');
-// 		// has <ul>
-// 		// cy.contains('Log in');
-// 	});
-// });
+// Routes:
+// home newbie: login/sign-up button, all recipes
+describe('Homepage for newbies', () => {
+	it('Displays Sign up and Log in', () => {
+		cy.visit('/');
+		//has <a href="">
+		cy.contains('Sign up');
+		cy.contains('Log in');
+	});
+	it('Shows all recipes', () => {
+		// cy.visit('/');
+		cy.contains('Strawberry Jam');
+		// has <ul>
+		// cy.contains('Log in');
+	});
+});
 
-// // signUp
-// describe('Can sign up', () => {
-// 	it('Can go to Sign Up page', () => {
-// 		cy.visit('/');
-// 		cy.contains('Sign up').click();
-// 		cy.url().should('include', '/signUp');
-// 	});
-// 	it('Can create a new user', () => {
-// 		// cy.visit('/');
-// 		// cy.contains('Sign up').click();
-// 		// cy.url().should('include', '/signUp');
-// 		cy.get('input[name="name"]').type('Betty');
-// 		cy.get('input[name="email"]').type('be@tty.com');
-// 		cy.get('input[name="password"]').type('12345');
-// 		// cy.get('input[name="avatar"]').type('');
-// 		cy.get('button').click();
-// 		cy.contains('Log out');
-// 	});
-// 	it('Can find new user in db', () => {});
-// });
+// signUp
+describe('Can sign up', () => {
+	it('Can go to Sign Up page', () => {
+		cy.visit('/');
+		cy.contains('Sign up').click();
+		cy.url().should('include', '/signUp');
+	});
+	it('Can create a new user', () => {
+		// cy.visit('/');
+		// cy.contains('Sign up').click();
+		// cy.url().should('include', '/signUp');
+		cy.get('input[name="name"]').type('Betty');
+		cy.get('input[name="email"]').type('be@tty.com');
+		cy.get('input[name="password"]').type('12345');
+		// cy.get('input[name="avatar"]').type('');
+		cy.get('button').click();
+		cy.contains('Log out');
+	});
+	it('Can find new user in db', () => {});
+});
 
-// // // logIn
-// describe('Can sign up, log out, log in', () => {
-// 	it('Can create & log into new account', () => {
-// 		cy.visit('/');
-// 		cy.contains('Sign up').click();
-// 		cy.url().should('include', '/signUp');
-// 		cy.get('input[name="name"]').type('Betty');
-// 		cy.get('input[name="email"]').type('be@tty.com');
-// 		cy.get('input[name="password"]').type('12345');
-// 		// cy.get('input[name="avatar"]').type('');
-// 		cy.get('button').click();
-// 		cy.contains('Log out').click();
-// 		cy.contains('Log in').click();
-// 		cy.url().should('include', '/logIn');
-// 		cy.get('input[name="email"]').type('be@tty.com');
-// 		cy.get('input[name="password"]').type('12345');
-// 		cy.get('button').click();
-// 		cy.contains('Log out');
-// 		// cy.contains('Log in');
-// 	});
-// });
+// // logIn
+describe('Can sign up, log out, log in', () => {
+	it('Can create & log into new account', () => {
+		cy.visit('/');
+		cy.contains('Sign up').click();
+		cy.url().should('include', '/signUp');
+		cy.get('input[name="name"]').type('Betty');
+		cy.get('input[name="email"]').type('be@tty.com');
+		cy.get('input[name="password"]').type('12345');
+		// cy.get('input[name="avatar"]').type('');
+		cy.get('button').click();
+		cy.contains('Log out').click();
+		cy.contains('Log in').click();
+		cy.url().should('include', '/logIn');
+		cy.get('input[name="email"]').type('be@tty.com');
+		cy.get('input[name="password"]').type('12345');
+		cy.get('button').click();
+		cy.contains('Log out');
+		// cy.contains('Log in');
+	});
+});
 
-// // // home (member)
-// describe('Homepage for logged-in members', () => {
-// 	it('Can show links to write Recipes, edit Profile, log out', () => {
-// 		cy.visit('/');
-// 		// Create a user
-// 		cy.contains('Sign up').click();
-// 		cy.url().should('include', '/signUp');
-// 		cy.get('input[name="name"]').type('Betty');
-// 		cy.get('input[name="email"]').type('be@tty.com');
-// 		cy.get('input[name="password"]').type('12345');
-// 		cy.get('button').click();
-// 		// home for members
-// 		cy.contains('Log out');
-// 		cy.contains('My profile');
-// 		cy.contains('my recipes');
-// 		cy.contains('Write');
-// 		cy.contains('Strawberry Jam');
-// 	});
-// });
+// // home (member)
+describe('Homepage for logged-in members', () => {
+	it('Can show links to write Recipes, edit Profile, log out', () => {
+		cy.visit('/');
+		// Create a user
+		cy.contains('Sign up').click();
+		cy.url().should('include', '/signUp');
+		cy.get('input[name="name"]').type('Betty');
+		cy.get('input[name="email"]').type('be@tty.com');
+		cy.get('input[name="password"]').type('12345');
+		cy.get('button').click();
+		// home for members
+		cy.contains('Log out');
+		cy.contains('My profile');
+		cy.contains('my recipes');
+		cy.contains('Write');
+		cy.contains('Strawberry Jam');
+	});
+});
 
 // // recipeWrite
 describe('Can submit a new recipe', () => {
