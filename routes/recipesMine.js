d6d4e048c5e993339fe20673e14c9d7b67831853
jihye <jiji.pak41@gmail.com>
2021-10-21 21:09:34 +0100

    style recipeMine for a bigger screen and style form
    Co-authored-by: moggach <43313455+Moggach@users.noreply.github.com>
    Co-authored-by: Michael <72146072+MJOW1999@users.noreply.github.com>
    Co-authored-by: Barbara <77367593+0bubbles0@users.noreply.github.com>

--- routes/recipesMine.js
+++ routes/recipesMine.js
@@ -3,20 +3,20 @@ const auth = require('../auth.js');
 const { buildPage } = require('../template.js');
 
 function get(request, response) {
-	// SELECT all FROM recipes where user_id= myID (sid -> getSession -> email -> users -> id)
-	const sid = request.signedCookies.sid;
-	const title = `My Recipes`;
-	model
-		.getSession(sid)
-		.then((session) => {
-			console.log(session);
-			return session.user.email;
-		})
-		.then((userEmail) => model.showMyRecipes(userEmail))
-		.then((data) => {
-			return data
-				.map(
-					(recipe) => /*html*/ `
+  // SELECT all FROM recipes where user_id= myID (sid -> getSession -> email -> users -> id)
+  const sid = request.signedCookies.sid;
+  const title = `My Recipes`;
+  model
+    .getSession(sid)
+    .then((session) => {
+      console.log(session);
+      return session.user.email;
+    })
+    .then((userEmail) => model.showMyRecipes(userEmail))
+    .then((data) => {
+      return data
+        .map(
+          (recipe) => /*html*/ `
             <article>
             <h3>${recipe.title}</h3>
             <p>Author: ${recipe.name}</p>
@@ -24,43 +24,43 @@ function get(request, response) {
               <li>Ingredients
               <ul class="li-style">
               ${recipe.ingredients
-								.split(', ')
-								.map((x) => `<li>${x}</li>`)
-								.join('')}
+                .split(', ')
+                .map((x) => `<li>${x}</li>`)
+                .join('')}
               </ul>
               </li>
               <li>Instructions 
               <ul class="li-style">
               ${recipe.instructions
-								.split('. ')
-								.map((x) => `<li>${x}</li>`)
-								.join('')}
+                .split('. ')
+                .map((x) => `<li>${x}</li>`)
+                .join('')}
               </ul>
               </li>
             </ul>
-            <form action="/recipeDelete" method="POST">
+            </article>
+            <form action="/recipeDelete" method="POST" class="form-no-style centre">
                 <button>Delete my recipe</button>
             </form>
-            </article>
             `
-				)
-				.join('');
-		})
-		.then((recipeList) => {
-			const page = `<a href="/">Go Home</a> ${recipeList}`;
-			response.send(buildPage(title, page));
-		})
-		.catch((error) => {
-			console.error(error);
-			response.send(
-				buildPage(
-					`Error`,
-					/*html*/ `<h2>Couldn't show your recipes</h2> <div>
+        )
+        .join('');
+    })
+    .then((recipeList) => {
+      const page = `<a href="/">Go Home</a> ${recipeList}`;
+      response.send(buildPage(title, page));
+    })
+    .catch((error) => {
+      console.error(error);
+      response.send(
+        buildPage(
+          `Error`,
+          /*html*/ `<h2>Couldn't show your recipes</h2> <div>
 	      <a href="/">Go Back</a>
 	  </div>`
-				)
-			);
-		});
+        )
+      );
+    });
 }
 
 module.exports = { get };
