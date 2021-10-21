8b40dfedc1e68756b0d870072b052d2c38580db2
jihye <jiji.pak41@gmail.com>
2021-10-21 20:39:18 +0100

    style userEdit page - align form, change form style, change label and input style
    Co-authored-by: moggach <43313455+Moggach@users.noreply.github.com>
    Co-authored-by: Michael <72146072+MJOW1999@users.noreply.github.com>
    Co-authored-by: Barbara <77367593+0bubbles0@users.noreply.github.com>

--- routes/userEdit.js
+++ routes/userEdit.js
@@ -2,54 +2,56 @@ const model = require('../database/model.js');
 const { buildPage } = require('../template.js');
 
 function get(request, response) {
-	const title = 'My details';
-	// Who is logged in right now?
-	const sid = request.signedCookies.sid;
-	model
-		.getSession(sid)
-		.then((session) => {
-			const content =
-				/*html */
-				`<form action="userEdit" method="POST">
+  const title = 'My details';
+  // Who is logged in right now?
+  const sid = request.signedCookies.sid;
+  model
+    .getSession(sid)
+    .then((session) => {
+      const content =
+        /*html */
+        `<form action="userEdit" method="POST" class="flex flex-column centre">
         <h3>Update my details</h3>
       <label for="name">Name <span aria-hidden="true">*</span></label>
       <input type="text" id="name" name="name" value="${session.user.name}" required />
       <label for="email">Email <span aria-hidden="true">*</span></label>
       <input type="text" id="email" name="email" value="${session.user.email}" required />
-    <button>Save Changes</button>
+    <button class="margin-centre">Save Changes</button>
     </form>
-    <form action="userDelete" method="POST">
-    <button>Delete my account</button>
-    </form>
-
-    <form action="/logOut" method="POST">
-    <button id="logoutBtn">Log out</button>
+		<section class ="manage-account">
+    <form action="/logOut" method="POST" class="form-no-style">
     <a href="/recipesMine">Show my recipes</a>
     <a href="/recipeWrite">Write a new recipe</a>
+    <button id="logoutBtn">Log out</button>
+		</form>
+    <form action="userDelete" method="POST" class="form-no-style">
+    <button>Delete my account</button>
+    </form>
+		</section>
     `;
-			response.send(buildPage(title, content));
-		})
-		.catch(() => response.send('error!'));
+      response.send(buildPage(title, content));
+    })
+    .catch(() => response.send('error!'));
 }
 
 function post(request, response) {
-	// Who is logged in?
-	const sid = request.signedCookies.sid;
+  // Who is logged in?
+  const sid = request.signedCookies.sid;
 
-	// look up email from current sid-data
-	return (
-		model
-			.getSession(sid)
-			.then((session) => session.user.email)
-			// Update users row for this email
-			.then((sessionEmail) => model.updateUser(sessionEmail, name))
-			.then(() => response.send('successfully updated'))
-			.catch((error) => {
-				console.warn(error);
-				response.send(`<h2>Sorry</h2>
+  // look up email from current sid-data
+  return (
+    model
+      .getSession(sid)
+      .then((session) => session.user.email)
+      // Update users row for this email
+      .then((sessionEmail) => model.updateUser(sessionEmail, name))
+      .then(() => response.send('successfully updated'))
+      .catch((error) => {
+        console.warn(error);
+        response.send(`<h2>Sorry</h2>
           <div class="centre"><a href="/">Go Home</a>`);
-			})
-	);
+      })
+  );
 }
 
 module.exports = { get, post };
