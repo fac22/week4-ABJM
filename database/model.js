0ddfbe170380680ad618cb4757072c67f05cf768
“Moggach” <acunnane13@gmail.com>
2021-10-22 11:52:56 +0100

    reversing order of recipes displayed - most recently added first

--- database/model.js
+++ database/model.js
@@ -2,82 +2,82 @@ const db = require('./connection.js');
 
 // Users
 function createUser(name, email, hash, avatar) {
-	const INSERT_USER = `INSERT INTO users (name, email, password, avatar) VALUES ($1, $2, $3, $4) 
+  const INSERT_USER = `INSERT INTO users (name, email, password, avatar) VALUES ($1, $2, $3, $4) 
   RETURNING id, name, email;`;
-	return db
-		.query(INSERT_USER, [name, email, hash, avatar])
-		.then((result) => {
-			return result.rows[0];
-		})
-		.catch(console.warn);
+  return db
+    .query(INSERT_USER, [name, email, hash, avatar])
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch(console.warn);
 }
 
 function getUser(email) {
-	const SELECT_USER = `
+  const SELECT_USER = `
     SELECT id, email, password, name, avatar FROM users WHERE email=$1
     `;
-	return db
-		.query(SELECT_USER, [email])
-		.then((result) => result.rows[0])
-		.catch(console.warn);
+  return db
+    .query(SELECT_USER, [email])
+    .then((result) => result.rows[0])
+    .catch(console.warn);
 }
 
 function getAvatar(id) {
-	return db
-		.query('SELECT avatar FROM users WHERE id=$1', [id])
-		.then((result) => result.rows[0])
-		.catch(console.warn);
+  return db
+    .query('SELECT avatar FROM users WHERE id=$1', [id])
+    .then((result) => result.rows[0])
+    .catch(console.warn);
 }
 
 function updateUser(sessionEmail, name) {
-	const UPDATER = `UPDATE users SET name = $1 WHERE users.email = $2;`;
-	return db
-		.query(UPDATER, [name, sessionEmail])
-		.then((result) => result.rows[0])
-		.catch(console.warn);
+  const UPDATER = `UPDATE users SET name = $1 WHERE users.email = $2;`;
+  return db
+    .query(UPDATER, [name, sessionEmail])
+    .then((result) => result.rows[0])
+    .catch(console.warn);
 }
 
 function deleteUser(email) {
-	const DELETE_USER = `DELETE from users WHERE email = $1;`;
-	return db.query(DELETE_USER, [email]).catch(console.warn);
+  const DELETE_USER = `DELETE from users WHERE email = $1;`;
+  return db.query(DELETE_USER, [email]).catch(console.warn);
 }
 
 // Sessions
 function createSession(sid, json) {
-	const INSERT_SESSION = `INSERT INTO sessions (sid, data) VALUES ($1, $2)
+  const INSERT_SESSION = `INSERT INTO sessions (sid, data) VALUES ($1, $2)
 	RETURNING sid;`;
-	return db
-		.query(INSERT_SESSION, [sid, json])
-		.then((response) => response.rows[0].sid)
-		.catch(console.warn);
+  return db
+    .query(INSERT_SESSION, [sid, json])
+    .then((response) => response.rows[0].sid)
+    .catch(console.warn);
 }
 
 function getSession(sid) {
-	const GET_SESSION = `SELECT data FROM sessions WHERE sid = $1`;
-	return db
-		.query(GET_SESSION, [sid])
-		.then((result) => {
-			const singleResult = result.rows[0];
-			return singleResult && singleResult.data;
-		})
-		.catch(console.warn);
+  const GET_SESSION = `SELECT data FROM sessions WHERE sid = $1`;
+  return db
+    .query(GET_SESSION, [sid])
+    .then((result) => {
+      const singleResult = result.rows[0];
+      return singleResult && singleResult.data;
+    })
+    .catch(console.warn);
 }
 
 function deleteSession(sid) {
-	const DELETE_SESSION = 'DELETE FROM sessions WHERE sid=$1';
-	return db.query(DELETE_SESSION, [sid]).catch(console.warn);
+  const DELETE_SESSION = 'DELETE FROM sessions WHERE sid=$1';
+  return db.query(DELETE_SESSION, [sid]).catch(console.warn);
 }
 
 // Recipes
 function getRecipes() {
-	return db
-		.query('SELECT * FROM recipes WHERE id=$1', [id])
-		.then((result) => result.rows[0])
-		.catch(console.warn);
+  return db
+    .query('SELECT * FROM recipes WHERE id=$1', [id])
+    .then((result) => result.rows[0])
+    .catch(console.warn);
 }
 
 function showRecipes() {
-	const query = `SELECT 
+  const query = `SELECT 
   recipes.title, 
   recipes.ingredients, 
   recipes.instructions,
@@ -85,14 +85,14 @@ function showRecipes() {
 FROM recipes 
 JOIN users 
 ON recipes.user_id = users.id`;
-	return db
-		.query(query)
-		.then((result) => result.rows)
-		.catch(console.warn);
+  return db
+    .query(query)
+    .then((result) => result.rows.reverse())
+    .catch(console.warn);
 }
 
 function showMyRecipes(email) {
-	const query = `
+  const query = `
   SELECT 
     recipes.title, 
     recipes.ingredients, 
@@ -103,10 +103,10 @@ function showMyRecipes(email) {
   ON recipes.user_id = users.id
   WHERE email = $1
   `;
-	return db
-		.query(query, [email])
-		.then((result) => result.rows)
-		.catch(console.warn);
+  return db
+    .query(query, [email])
+    .then((result) => result.rows.reverse())
+    .catch(console.warn);
 }
 
 /*function updateRecipe(sessionRecipe, name) {
@@ -117,23 +117,23 @@ function showMyRecipes(email) {
 }*/
 
 function deleteRecipe(title) {
-	console.log('title being passed to delete Recipe function', title);
-	const DELETE_RECIPE = `DELETE FROM recipes WHERE title=$1`;
-	console.log('deleting recipe');
-	return db.query(DELETE_RECIPE, [title]).catch(console.warn);
+  console.log('title being passed to delete Recipe function', title);
+  const DELETE_RECIPE = `DELETE FROM recipes WHERE title=$1`;
+  console.log('deleting recipe');
+  return db.query(DELETE_RECIPE, [title]).catch(console.warn);
 }
 
 module.exports = {
-	createUser,
-	getUser,
-	getAvatar,
-	updateUser,
-	deleteUser,
-	createSession,
-	getSession,
-	deleteSession,
-	showRecipes,
-	showMyRecipes,
-	getRecipes,
-	deleteRecipe,
+  createUser,
+  getUser,
+  getAvatar,
+  updateUser,
+  deleteUser,
+  createSession,
+  getSession,
+  deleteSession,
+  showRecipes,
+  showMyRecipes,
+  getRecipes,
+  deleteRecipe,
 };
