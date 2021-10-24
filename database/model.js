ab5e47c337c7418089f2147b56a7926451608dec
jihye <jiji.pak41@gmail.com>
2021-10-25 00:01:41 +0100

    fix user edit page

--- database/model.js
+++ database/model.js
@@ -4,34 +4,42 @@ const db = require('./connection.js');
 function createUser(name, email, hash, avatar) {
   const INSERT_USER = `INSERT INTO users (name, email, password, avatar) VALUES ($1, $2, $3, $4) 
   RETURNING id, name, email;`;
-  return db.query(INSERT_USER, [name, email, hash, avatar]).then((result) => {
-    return result.rows[0];
-  });
+  return db
+    .query(INSERT_USER, [name, email, hash, avatar])
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch(console.warn);
 }
 
 function getUser(email) {
   const SELECT_USER = `
     SELECT id, email, password, name, avatar FROM users WHERE email=$1
     `;
-  return db.query(SELECT_USER, [email]).then((result) => result.rows[0]);
+  return db
+    .query(SELECT_USER, [email])
+    .then((result) => result.rows[0])
+    .catch(console.warn);
 }
 
 function getAvatar(id) {
   return db
     .query('SELECT avatar FROM users WHERE id=$1', [id])
-    .then((result) => result.rows[0]);
+    .then((result) => result.rows[0])
+    .catch(console.warn);
 }
 
 function updateUser(sessionEmail, name) {
   const UPDATER = `UPDATE users SET name = $1 WHERE users.email = $2;`;
   return db
     .query(UPDATER, [name, sessionEmail])
-    .then((result) => result.rows[0]);
+    .then((result) => result.rows[0])
+    .catch(console.warn);
 }
 
 function deleteUser(email) {
   const DELETE_USER = `DELETE from users WHERE email = $1;`;
-  return db.query(DELETE_USER, [email]);
+  return db.query(DELETE_USER, [email]).catch(console.warn);
 }
 
 // Sessions
@@ -40,15 +48,19 @@ function createSession(sid, json) {
 	RETURNING sid;`;
   return db
     .query(INSERT_SESSION, [sid, json])
-    .then((response) => response.rows[0].sid);
+    .then((response) => response.rows[0].sid)
+    .catch(console.warn);
 }
 
 function getSession(sid) {
   const GET_SESSION = `SELECT data FROM sessions WHERE sid = $1`;
-  return db.query(GET_SESSION, [sid]).then((result) => {
-    const singleResult = result.rows[0];
-    return singleResult && singleResult.data;
-  });
+  return db
+    .query(GET_SESSION, [sid])
+    .then((result) => {
+      const singleResult = result.rows[0];
+      return singleResult.data;
+    })
+    .catch(console.warn);
 }
 
 function deleteSession(sid) {
@@ -60,7 +72,8 @@ function deleteSession(sid) {
 function getRecipes() {
   return db
     .query('SELECT * FROM recipes WHERE id=$1', [id])
-    .then((result) => result.rows[0]);
+    .then((result) => result.rows[0])
+    .catch(console.warn);
 }
 
 function showRecipes() {
@@ -72,7 +85,10 @@ function showRecipes() {
 FROM recipes 
 JOIN users 
 ON recipes.user_id = users.id`;
-  return db.query(query).then((result) => result.rows);
+  return db
+    .query(query)
+    .then((result) => result.rows)
+    .catch(console.warn);
 }
 
 function showMyRecipes(email) {
@@ -87,7 +103,10 @@ function showMyRecipes(email) {
   ON recipes.user_id = users.id
   WHERE email = $1
   `;
-  return db.query(query, [email]).then((result) => result.rows);
+  return db
+    .query(query, [email])
+    .then((result) => result.rows)
+    .catch(console.warn);
 }
 
 /*function updateRecipe(sessionRecipe, name) {
@@ -101,7 +120,7 @@ function deleteRecipe(title) {
   console.log('title being passed to delete Recipe function', title);
   const DELETE_RECIPE = `DELETE FROM recipes WHERE title=$1`;
   console.log('deleting recipe');
-  return db.query(DELETE_RECIPE, [title]);
+  return db.query(DELETE_RECIPE, [title]).catch(console.warn);
 }
 
 module.exports = {
--- routes/userEdit.js
+++ routes/userEdit.js
@@ -12,7 +12,62 @@ function get(request, response) {
         /*html */
         `<form action="userEdit" method="POST" class="flex flex-column centre">
         <h3>Update my details</h3>
-      <label for="name">Name <span aria-hidden="true">*</span></label>
+      <label for=const model = require('../database/model.js');
+      const { buildPage } = require('../template.js');
+      
+      function get(request, response) {
+        const title = 'My details';
+        // Who is logged in right now?
+        const sid = request.signedCookies.sid;
+        model
+          .getSession(sid)
+          .then((session) => {
+            const content =
+              /*html */
+              `<form action="userEdit" method="POST" class="flex flex-column centre">
+              <h3>Update my details</h3>
+            <label for="name">Name <span aria-hidden="true">*</span></label>
+            <input type="text" id="name" name="name" value="${session.user.name}" required />
+            <label for="email">Email <span aria-hidden="true">*</span></label>
+            <input type="text" id="email" name="email" value="${session.user.email}" required />
+          <button class="margin-centre">Save Changes</button>
+          </form>
+          <section class ="manage-account">
+          <form action="/logOut" method="POST" class="form-no-style">
+          <a href="/recipesMine">Show my recipes</a>
+          <a href="/recipeWrite">Write a new recipe</a>
+          <button id="logoutBtn">Log out</button>
+          </form>
+          <form action="userDelete" method="POST" class="form-no-style">
+          <button>Delete my account</button>
+          </form>
+          </section>
+          `;
+            response.send(buildPage(title, content));
+          })
+          .catch(() => response.send('error!'));
+      }
+      
+      function post(request, response) {
+        // Who is logged in?
+        const sid = request.signedCookies.sid;
+      
+        // look up email from current sid-data
+        return model
+          .getSession(sid)
+          .then((session) => {
+            return model.updateUser(session.user.email, session.user.name);
+          })
+          .then(() => response.send('successfully updated'))
+          .catch((error) => {
+            console.warn(error);
+            response.send(`<h2>Sorry</h2>
+                <div class="centre"><a href="/">Go Home</a>`);
+          });
+      }
+      
+      module.exports = { get, post };
+      "name">Name <span aria-hidden="true">*</span></label>
       <input type="text" id="name" name="name" value="${session.user.name}" required />
       <label for="email">Email <span aria-hidden="true">*</span></label>
       <input type="text" id="email" name="email" value="${session.user.email}" required />
@@ -39,19 +94,17 @@ function post(request, response) {
   const sid = request.signedCookies.sid;
 
   // look up email from current sid-data
-  return (
-    model
-      .getSession(sid)
-      .then((session) => session.user.email)
-      // Update users row for this email
-      .then((sessionEmail) => model.updateUser(sessionEmail, name))
-      .then(() => response.send('successfully updated'))
-      .catch((error) => {
-        console.warn(error);
-        response.send(`<h2>Sorry</h2>
+  return model
+    .getSession(sid)
+    .then((session) => {
+      return model.updateUser(session.user.email, session.user.name);
+    })
+    .then(() => response.send('successfully updated'))
+    .catch((error) => {
+      console.warn(error);
+      response.send(`<h2>Sorry</h2>
           <div class="centre"><a href="/">Go Home</a>`);
-      })
-  );
+    });
 }
 
 module.exports = { get, post };
